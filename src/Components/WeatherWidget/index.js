428eef29c650b00dbd10e8e2190d3c10ddbb8200
Osee-Ralantoarison <o.ralanto@gmail.com>
2021-08-06 13:43:33 +0200

    Weather app

--- src/Components/WeatherWidget/index.js
+++ src/Components/WeatherWidget/index.js
@@ -1,15 +1,24 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { makeStyles } from "@material-ui/core/styles";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Input from "@material-ui/core/Input";
+import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
-import CardHeader from "@material-ui/core/CardHeader";
-import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
+import {
+  WiDaySunny,
+  WiCloud,
+  WiRain,
+  WiDayCloudyHigh,
+} from "weather-icons-react";
 import "./style.css";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: 345,
+    margin: "auto",
   },
   media: {
     height: 0,
@@ -19,7 +28,12 @@ const useStyles = makeStyles((theme) => ({
 
 const WeatherWidget = () => {
   const [city, setCity] = useState("Paris");
-  const [temperature, setTemperature] = useState();
+  const [input, setInput] = useState();
+  const [temp, setTemp] = useState();
+  const [tempMax, setTempMax] = useState();
+  const [tempMin, setTempMin] = useState();
+  const [feelsLike, setFeelsLike] = useState();
+  const [humidity, setHumidity] = useState();
   const [icon, setIcon] = useState();
   const classes = useStyles();
 
@@ -29,41 +43,70 @@ const WeatherWidget = () => {
     const url = `${baseUrl}?q=${city}&appid=${apiKey}&units=metric`;
 
     axios.get(url).then((response) => {
-      console.log("response", response);
-      const temp = Math.round(response.data.main.temp);
-      setTemperature(temp);
-      setIcon(response.data.weather[0].icon);
+      console.log(response.data.weather[0].main);
+      setTemp(response.data.main.temp);
+      setTempMax(response.data.main.temp_max);
+      setTempMin(response.data.main.temp_min);
+      setFeelsLike(response.data.main.feels_like);
+      setHumidity(response.data.main.humidity);
+      setIcon(response.data.weather[0].main);
     });
   }, [city]);
 
+  const inputChange = (e) => {
+    e.preventDefault();
+    setInput(e.target.value);
+  };
+
+  const onClick = (e) => {
+    e.preventDefault();
+    setCity(input);
+  };
+
+  const icons = () => {
+    switch (icon) {
+      case "Clear":
+        return <WiDaySunny size={200} color="#FFD760" />;
+      case "Clouds":
+        return <WiCloud size={200} color="#c0bdcc" />;
+      case "Rain":
+        return <WiRain size={200} color="#5328ff" />;
+      default:
+        return <WiDayCloudyHigh size={200} color="#d5e08f" />;
+    }
+  };
   return (
-    <div>
-      <Card className={`${classes.root} widget`}>
-        <CardHeader title="Weather widget" />
+    <div className="weatherApp">
+      <div className="mb2">
+        <FormControl>
+          <InputLabel htmlFor="my-input">Entrer votre ville</InputLabel>
+          <Input
+            id="my-input"
+            aria-describedby="my-helper-text"
+            className="mb2 color"
+            onChange={(e) => inputChange(e)}
+          />
+          <Button variant="outlined" size="medium" onClick={(e) => onClick(e)}>
+            Valider
+          </Button>
+        </FormControl>
+      </div>
+      <Card className={`${classes.root} weather`}>
         <CardContent>
           <h2>{city}</h2>
         </CardContent>
-        <CardMedia
-          className={classes.media}
-          image={`http://openweathermap.org/img/wn/${icon}@2x.png`}
-          title="Meteo"
-        />
+        {icons()}
         <CardContent>
-          <h2>{temperature}°</h2>
+          <h2>Actuel : {Math.round(temp)}°</h2>
+          <h2>Max : {Math.round(tempMax)}°</h2>
+          <h2>Min : {Math.round(tempMin)}°</h2>
         </CardContent>
+        <p>Recentie : {Math.round(feelsLike)}°</p>
+        <p>Humidité : {Math.round(humidity)}%</p>
+        <CardContent></CardContent>
       </Card>
     </div>
   );
 };
-
-// if (temperature === undefined) {
-//   return (
-//     <div className="weather-widget">
-//       <div className="weather-widget__loading">
-//         <p>Loading...</p>
-//       </div>
-//     </div>
-//   );
-// }
 
 export default WeatherWidget;
